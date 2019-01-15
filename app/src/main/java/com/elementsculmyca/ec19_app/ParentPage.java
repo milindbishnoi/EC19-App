111934f9354e31407c962bed4c8cc9816d6be131
shubham0008 <sharma.shubham0008@gmail.com>
2019-01-15 23:49:34 +0530

    hello

---
+++ app/src/main/java/com/elementsculmyca/ec19_app/ParentPage.java
@@ -0,0 +1,13 @@
+package com.elementsculmyca.ec19_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ParentPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_page);
+    }
+}
