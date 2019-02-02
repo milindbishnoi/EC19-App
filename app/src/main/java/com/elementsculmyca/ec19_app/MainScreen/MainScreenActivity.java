6cceb0605ef5fe08159979e96b16817b5834f1ec
shubham0008 <sharma.shubham0008@gmail.com>
2019-02-02 22:47:25 +0530

    branch commit

---
+++ app/src/main/java/com/elementsculmyca/ec19_app/MainScreen/MainScreenActivity.java
@@ -0,0 +1,17 @@
+package com.elementsculmyca.ec19_app.MainScreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.elementsculmyca.ec19_app.R;
+
+
+public class MainScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen2);
+
+    }
+}
