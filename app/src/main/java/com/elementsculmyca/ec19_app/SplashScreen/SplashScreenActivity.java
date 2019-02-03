9836d15967af7b2307b74565505f5c946058aeff
milindbishnoi <milind.curio@gmail.com>
2019-02-03 13:44:59 +0530

    layout

--- app/src/main/java/com/elementsculmyca/ec19_app/SplashScreen/SplashScreenActivity.java
+++ app/src/main/java/com/elementsculmyca/ec19_app/SplashScreen/SplashScreenActivity.java
@@ -1,17 +1,29 @@
 package com.elementsculmyca.ec19_app.SplashScreen;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+
+import com.elementsculmyca.ec19_app.LoginScreen.LoginPresenter;
 import com.elementsculmyca.ec19_app.R;
 
 public class SplashScreenActivity extends Activity {
-
+    public static int SPLASH_TIME_OUT=4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent SplashScreen =new Intent(SplashScreenActivity.this,LoginPresenter.class);
+                startActivity(SplashScreen);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
 
-        //Jai Mata DI
     }
 }
+//jai mata di
