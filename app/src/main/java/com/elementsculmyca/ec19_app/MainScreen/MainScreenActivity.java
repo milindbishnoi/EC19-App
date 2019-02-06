0725d5719989500837a08a53ef7229de5f38a589
sayantanudey <sayantanu.dey@gmail.com>
2019-02-06 19:42:57 +0530

    navigation bar

--- app/src/main/java/com/elementsculmyca/ec19_app/MainScreen/MainScreenActivity.java
+++ app/src/main/java/com/elementsculmyca/ec19_app/MainScreen/MainScreenActivity.java
@@ -2,16 +2,66 @@ package com.elementsculmyca.ec19_app.MainScreen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.elementsculmyca.ec19_app.R;
 
 
-public class MainScreenActivity extends Activity {
-
+public class MainScreenActivity extends AppCompatActivity{
+    private DrawerLayout d1;
+    private ActionBarDrawerToggle abdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen2);
+        setContentView(R.layout.activity_main_screen);
+        d1=(DrawerLayout)findViewById(R.id.d1);
+        abdt=new ActionBarDrawerToggle(this,d1,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+        d1.addDrawerListener(abdt);
+        abdt.syncState();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        final NavigationView nav=(NavigationView)findViewById(R.id.nav_view);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id= item.getItemId();
+                if(id==R.id.i1)
+                {
+                    Toast.makeText(MainScreenActivity.this, "Event 1 clicked", Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.i2)
+                {
+                    Toast.makeText(MainScreenActivity.this, "Event 2 clicked", Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.i3)
+                {
+                    Toast.makeText(MainScreenActivity.this, "Event 3 clicked", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
 
+                d1.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
