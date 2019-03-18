f3c1f87f445587b532252becafa62da2f185f7f4
milindbishnoi <milind.curio@gmail.com>
2019-03-18 22:10:23 +0530

    Single_event Activity layout

---
+++ app/src/main/java/com/elementsculmyca/ec19_app/Single_event.java
@@ -0,0 +1,13 @@
+package com.elementsculmyca.ec19_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Single_event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_event);
+    }
+}
