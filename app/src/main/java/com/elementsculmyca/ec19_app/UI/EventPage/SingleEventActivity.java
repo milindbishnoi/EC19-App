7695fad63be71be35ca5ab6d41110869ad36649f
rahulydav <rahulrocks9915@gmail.com>
2019-03-23 02:13:45 +0530

    Revert "info retrieved on SingleEventActivity and DescriptionEventFragment"
    This reverts commit 4a03d8b

--- app/src/main/java/com/elementsculmyca/ec19_app/UI/EventPage/SingleEventActivity.java
+++ app/src/main/java/com/elementsculmyca/ec19_app/UI/EventPage/SingleEventActivity.java
@@ -17,12 +17,10 @@ import com.elementsculmyca.ec19_app.R;
 import com.elementsculmyca.ec19_app.UI.SplashScreen.SplashScreenActivity;
 
 public class SingleEventActivity extends AppCompatActivity {
-    TextView registerButton,eventName,eventDesc,eventVenue,eventDay;
+    TextView registerButton,eventName;
     ImageView sharebutton;
     EventsDao_Impl dao;
-    private String eventId,eventClubName,eventCatogery,eventRules,eventPhotoLink,eventCoordinator,eventPrize,eventType,eventTags;
-    private int eventFee,eventHitCount;
-    private long eventStartTime,eventEndTime;
+    private String eventId;
     EventLocalModel eventData;
 
     @Override
@@ -30,9 +28,6 @@ public class SingleEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_event);
         eventName=findViewById(R.id.event_title);
-        eventDesc = findViewById(R.id.event_desc);
-        eventVenue = findViewById(R.id.event_venue);
-        eventDay = findViewById(R.id.event_day);
         registerButton = findViewById(R.id.register);
         sharebutton = findViewById(R.id.share_event);
         dao=new EventsDao_Impl(AppDatabase.getAppDatabase(SingleEventActivity.this));
@@ -58,27 +53,7 @@ public class SingleEventActivity extends AppCompatActivity {
         }
         //add data to page
         eventName.setText(eventData.getTitle());
-        eventId = eventData.getId();
-        eventClubName = eventData.getClubname();
-        eventCatogery = eventData.getCategory();
-        eventDesc.setText(eventData.getDesc());
-        eventRules = eventData.getRules();
-        eventVenue.setText(eventData.getVenue());
-        eventPhotoLink = eventData.getPhotolink();
-        eventFee = eventData.getFee();
-        eventStartTime = eventData.getStartTime();
-        eventEndTime = eventData.getEndTime();
-        eventCoordinator = eventData.getCoordinator();
-        eventPrize = eventData.getPrizes();
-        eventType = eventData.getEventType();
-        eventDay.setText(eventData.getDay());
-        eventTags = eventData.getTags();
-        eventHitCount = eventData.getHitcount();
 
-        Bundle bundle = new Bundle();
-        bundle.putString("id",eventId);
-        DescriptionEventFragment descriptionEventFragment = new DescriptionEventFragment();
-        descriptionEventFragment.setArguments(bundle);
 
 
         FragmentManager manager = getSupportFragmentManager();
